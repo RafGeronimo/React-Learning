11f65e0006f1ef94d43580641f22ea0058f19f18
RafGeronimo <rafaelgeronimo96@gmail.com>
2023-07-26 15:29:40 -0300

    atualização de display da calculadora

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -5,7 +5,7 @@ export default (props) => {
   let classes = "button ";
   classes += props.operation ? "operation" : "";
   classes += props.double ? "double" : "";
-  classes += props.triple ? "triple" : ""
+  classes += props.triple ? "triple" : "";
 
   return (<button className={classes} onClick={e => props.click && props.click(props.label)}>{props.label}
     </button>
