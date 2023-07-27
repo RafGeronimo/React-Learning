924ead549576ba9509f36201e1760079f7e3e1e9
RafGeronimo <rafaelgeronimo96@gmail.com>
2023-07-27 14:32:34 -0300

    projeto navegacao

---
+++ navegacao/src/components/layout/Content.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Content.css"
+const Content = (props) => {
+    return (
+        <main className="Content">Componente Content</main>
+    )
+}
+
+export default Content
---
+++ navegacao/src/views/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = (props) => {
+    return (
+        <div className="App">Componente App</div>
+    )
+}
+
+export default App
